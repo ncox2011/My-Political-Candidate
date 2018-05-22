23e47b1396ccb5ea4417f60c2e8bd2dbb6205e66
Natasha Cox <natashac1014@gmail.com>
2018-05-22 15:00:24 -0500

    fixed code

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var x = document.getElementsByTagName("ARTICLE")[0];
 var y = document.createTextNode("My mission is a bunch of political garb.")
 x.appendChild(y);
+
+var section = document.getElementsByTagName("section")[0];
+var att = document.createAttribute("class");
+att.value = "demo";
+section.setAttributeNode(att);
 console.log(x);
 
